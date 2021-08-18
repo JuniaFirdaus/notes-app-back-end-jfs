f7b74f5bfeee81dc405b5f963c8bfaa7a381e329
Junia Firdaus <firdausjunia@gmail.com>
2021-08-18 09:56:49 +0700

    postgres database implemented

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -4,42 +4,20 @@ const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
 const { mapDBToModel } = require('../../utils');
 const NotFoundError = require('../../exceptions/NotFoundError');
-const AuthorizationError = require('../../exceptions/AuthorizationError');
 
 class NotesService {
   constructor() {
     this._pool = new Pool();
   }
 
-  async verifyNoteOwner(id, owner) {
-    const query = {
-      text: 'SELECT * FROM notes WHERE id = $1',
-      values: [id],
-    };
-    const result = await this._pool.query(query);
-    if (!result.rows.length) {
-      throw new NotFoundError('Catatan tidak ditemukan');
-    }
-
-    const note = result.rows[0];
-    if (note.owner !== owner) {
-      throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
-    }
-  }
-
-  async addNote({
-    title,
-    body,
-    tags,
-    owner,
-  }) {
+  async addNote({ title, body, tags }) {
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
 
     const query = {
       text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
-      values: [id, title, body, tags, createdAt, updatedAt, owner],
+      values: [id, title, body, tags, createdAt, updatedAt],
     };
 
     const result = await this._pool.query(query);
@@ -51,12 +29,8 @@ class NotesService {
     return result.rows[0].id;
   }
 
-  async getNotes(owner) {
-    const query = {
-      text: 'SELECT * FROM notes WHERE owner = $1',
-      values: [owner],
-    };
-    const result = await this._pool.query(query);
+  async getNotes() {
+    const result = await this._pool.query('SELECT * FROM notes');
     return result.rows.map(mapDBToModel);
   }
 
