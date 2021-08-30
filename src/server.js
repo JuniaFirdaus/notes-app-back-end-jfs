56902deab6e3d0b3ac6478e757ee2bdc8c1f668a
Junia Firdaus <firdausjunia@gmail.com>
2021-08-30 10:32:55 +0700

    user registration implemented

---
+++ src/server.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+// notes
+// const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/postgres/NotesService');
+// const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
+
+// users
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
+const init = async () => {
+  const notesService = new NotesService();
+  const usersService = new UsersService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    // !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+  // try {
+  //   server.route(routes);
+  //   await server.start();
+  //   console.log(`Server berjalan pada ${server.info.uri}`);
+  // } catch (error) {
+  //   console.log(`Server not found ${error}`);
+  // }
+};
+
+init();
